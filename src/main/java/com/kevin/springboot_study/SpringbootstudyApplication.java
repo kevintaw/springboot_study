ba3f6da4325c0f5559427ff80299b19a35abf642
taoningbo <taoningbo@tudou.com>
2016-06-30 12:30:07 +0800

    新建springboot项目

---
+++ src/main/java/com/kevin/springboot_study/SpringbootstudyApplication.java
@@ -0,0 +1,15 @@
+package com.kevin.springboot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kevin on 16-6-30.
+ */
+
+@SpringBootApplication
+public class SpringbootstudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootstudyApplication.class, args);
+    }
+}
