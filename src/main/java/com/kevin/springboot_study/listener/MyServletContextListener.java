04011844a9c59d9525375a2013d2e43df983080a
taoningbo <taoningbo@tudou.com>
2016-07-05 11:58:42 +0800

    添加拦截器

--- src/main/java/com/kevin/springboot_study/listener/MyServletContextListener.java
+++ src/main/java/com/kevin/springboot_study/listener/MyServletContextListener.java
@@ -17,12 +17,12 @@ public class MyServletContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("ServletContex初始化");
+        System.out.println("ServletContext初始化");
         System.out.println(sce.getServletContext().getServerInfo());
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        System.out.println("ServletContex销毁");
+        System.out.println("ServletContext销毁");
     }
 }
